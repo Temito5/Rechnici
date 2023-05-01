7c6d31b762818f11a37f5d52508aa5dd42e50ab4
Staiko <Staiko@DESKTOP-NUVQ8TO>
2023-05-01 14:23:12 +0300

    Add project files.

---
+++ TelefonenUkazatel/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelefonenUkazatel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> kontakti = new Dictionary<string, string>();
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                string[] tokens = input.Split();
+                string command = tokens[0];
+                string name = tokens[1];
+
+                if (command=="A")
+                {
+                    string phone = tokens[2];
+                    kontakti.Add(name, phone);
+                }
+                else if (command=="S")
+                {
+                    if (kontakti.ContainsKey(name))
+                    {
+                        Console.WriteLine($"{name} -> {kontakti[name]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {name} does not exist.");
+                       
+
+                    }
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
